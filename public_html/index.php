3bd1c20adeec7dc103444bcc7e819164236b7a17
deziox <dezioxe@gmail.com>
2020-05-20 17:34:46 -0400

    PHP tutorials 2

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,21 @@
-<?php
-echo "Hello, World!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Danzel Test Site</title>
+</head>
+<body>
+    <h1>Header One</h1>
+    <?php
+        function worldGreet()
+        {
+            echo "Hello, World!";
+        }
+
+        if(array_key_exists('button1',$_POST)){
+            worldGreet();
+        }
+    ?>
+    <input type="submit" name="button1" class="button" value="Click Me"/>
+</body>
+</html>
