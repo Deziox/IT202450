d19f79b5fabc4fa03a366bbb579b95aa69f7c6f8
deziox <dezioxe@gmail.com>
2020-05-20 21:34:26 -0400

    Simple header

--- public_html/header.php
+++ public_html/header.php
@@ -4,8 +4,8 @@
 <div class="header">
     <a href="index.php" class="logo">battlio</a>
     <div class="header-right">
-        <a href="#home" class="active">home</a>
-        <a href="#login">login/register</a>
+        <a href="index.php">home</a>
+        <a href="#login" class="active">login/register</a>
         <a href="#howtoplay">how to play</a>
     </div>
 </div>
