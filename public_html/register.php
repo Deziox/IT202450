2a9d14be3ecb6eaedc6f98890dd826790dfb5483
deziox <dezioxe@gmail.com>
2020-05-20 20:32:54 -0400

    PHP tutorials 2 CI

--- public_html/register.php
+++ public_html/register.php
@@ -5,10 +5,8 @@
 <html>
     <head></head>
     <body>
-        <div>
-            <?php include('header.php'); ?>
-        </div>
-        <h1>REGISTER TODAY</h1>
+        <?php include('header.php'); ?>
+        <!--<h1>REGISTER TODAY</h1>-->
         <?php include('footer.php'); ?>
     </body>
 </html>
