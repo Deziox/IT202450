771f244cf87500c51b839dd9f17bc6f1f4333356
deziox <dezioxe@gmail.com>
2020-05-22 09:56:06 -0400

    page for registration

--- public_html/register.php
+++ public_html/register.php
@@ -1,8 +1,29 @@
+<?php
+    if(isset($_GET['submit'])){
+        echo $_GET['email'];
+
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head></head>
     <body>
         <?php include('header.php'); ?>
+        <section>
+            <h2>REGISTER AN ACCOUNT</h2>
+            <form action="register.php" method="get">
+                <label>Email:</label>
+                <input type="text" name="email">
+                <label>Username:</label>
+                <input type="text" name="username">
+                <label>Password:</label>
+                <input type="text" name="password">
+                <div>
+                    <input type="submit" name="submit" value="Register">
+                </div>
+            </form>
+        </section>
         <!--<h1>REGISTER TODAY</h1>-->
     </body>
 </html>
