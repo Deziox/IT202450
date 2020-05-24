b43ad26453374608a7cec5c03034e8fc03b38afb
deziox <dezioxe@gmail.com>
2020-05-24 14:14:09 -0400

    working on registration page

--- public_html/register.php
+++ public_html/register.php
@@ -1,31 +1,30 @@
 <?php
-//    if(isset($_GET['submit'])){
-//        echo $_GET['email'];
-//    }
+    $errors = array('email'=>'','username'=>'','password'=>'');
+
     if(isset($_POST['submit'])){
         if(empty($_POST['email'])){
-            echo "An email is required <br/>";
+            $errors['email'] = "An email is required";
         }else if(empty($_POST['username'])){
-            echo "A username is required";
+            $errors['username'] = "A username is required";
         }else if(empty($_POST['password'])){
-            echo "Password cannot be empty";
+            $errors['password'] = "Password cannot be empty";
         }else {
             $email = $_POST['email'];
             $username = $_POST['username'];
             $password = $_POST['password'];
 
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                echo "Not a Valid Email";
-            } else if (!preg_match("/^[a-zA-Z0-9]+$/", $username)) {
-                echo "Username cannot have any special symbols";
-            } else if (!preg_match("/^[A-Z].*[A-Z].*[A-Z]$/", $password) || preg_match("/^\s.*\s.*\s$/", $password)) {
-                echo "Password Must have at least one uppercase letter and no spaces";
-            } else{
-
-                echo htmlspecialchars($_POST['email']) . "\n";
-                echo htmlspecialchars($_POST['username']) . "\n";
-                echo htmlspecialchars($_POST['password']) . "\n";
+                $errors['email'] = "Not a Valid Email";
             }
+            if (!preg_match("/^[a-zA-Z0-9]+$/", $username)) {
+                $errors['username'] = "Username cannot have any special symbols";
+            }
+            if (!preg_match("/^[A-Z].*[A-Z].*[A-Z]$/", $password) || preg_match("/^\s.*\s.*\s$/", $password)) {
+                $errors['password'] = "Password Must have at least one uppercase letter and no spaces";
+            }
+                //echo htmlspecialchars($_POST['email']) . "\n";
+                //echo htmlspecialchars($_POST['username']) . "\n";
+                //echo htmlspecialchars($_POST['password']) . "\n";
         }
     }
 ?>
@@ -40,10 +39,13 @@
             <form action="register.php" method="post">
                 <label>Email:</label>
                 <input type="text" name="email">
+                <div class="error"><?php echo $errors['email'];?></div>
                 <label>Username:</label>
                 <input type="text" name="username">
+                <div class="error"><?php echo $errors['username'];?></div>
                 <label>Password:</label>
                 <input type="text" name="password">
+                <div class="error"><?php echo $errors['password'];?></div>
                 <div>
                     <input type="submit" name="submit" value="Register">
                 </div>
