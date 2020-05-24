8a15f3e5df64fe893c97d135d346af9de3240011
deziox <dezioxe@gmail.com>
2020-05-24 14:24:13 -0400

    working on registration page

--- public_html/register.php
+++ public_html/register.php
@@ -39,13 +39,13 @@
             <form action="register.php" method="post">
                 <label>Email:</label>
                 <div class="error"><?php echo $errors['email'];?></div>
-                <input type="text" name="email">
+                <input type="text" name="email"><br/>
                 <label>Username:</label>
                 <div class="error"><?php echo $errors['username'];?></div>
-                <input type="text" name="username">
+                <input type="text" name="username"><br/>
                 <label>Password:</label>
                 <div class="error"><?php echo $errors['password'];?></div>
-                <input type="text" name="password">
+                <input type="text" name="password"><br/>
                 <div>
                     <input type="submit" name="submit" value="Register">
                 </div>
