7f25ce87e63dbe46ed6945aadf2056368487b19f
deziox <dezioxe@gmail.com>
2020-05-24 08:54:07 -0400

    db connectivity test JawsDB

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -10,7 +10,7 @@
                                 email varchar(100) not null unique,
                                 created timestamp default current_timestamp,
                                 modified timestamp default current_timestamp on update current_timestamp,
-                                PRIMARY KEY ('id')        
+                                PRIMARY KEY (id)        
                             ) CHARACTER SET utf8 COLLATE utf8_general_ci");
         $r = $stmt->execute();
         echo var_export($stmt->errorInfo(),true);
