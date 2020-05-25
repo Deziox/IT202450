4a5b4e540f04e44ba5c1ab463cd95c92568e0cd6
deziox <dezioxe@gmail.com>
2020-05-25 09:51:54 -0400

    working on inserting registered users

--- public_html/header.php
+++ public_html/header.php
@@ -2,7 +2,7 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <div class="header">
-    <a href="index.php" class="logo"></a>
+    <a href="index.php" class="logo">a e s t h e t i c a</a>
     <div class="header-right">
         <a href="index.php">home</a>
         <a href="#login" class="active">login/register</a>
