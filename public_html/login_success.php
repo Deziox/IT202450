ec8e440d31d03f16a9ef802f24a2435e5d3cb4db
deziox <dezioxe@gmail.com>
2020-05-29 10:37:02 -0400

    aesthetica login page

--- public_html/login_success.php
+++ public_html/login_success.php
@@ -1,3 +1,8 @@
 <?php
-    echo "<h1>login successful</h1>"
+    if(isset($_SESSION['username'])) {
+        echo "<h1>login successful, welcome ".$_SESSION['username']."</h1>";
+        echo '<br /><br /><a href="logout.php">Logout</a>';
+    }else{
+        header("location:login.php");
+    }
 ?>
