84eb364bfd4f9e99484af8f4df7bc7561edb80c3
deziox <dezioxe@gmail.com>
2020-06-01 14:20:45 -0400

    User sessions

--- public_html/login_success.php
+++ public_html/login_success.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if(isset($_SESSION['username'])) {
+    if(isset($_SESSION['user'])) {
         echo "<h1>login successful, welcome ".$_SESSION['username']."</h1>";
         echo var_export($_SESSION,true);
         echo '<br /><br /><a href="logout.php">Logout</a>';
