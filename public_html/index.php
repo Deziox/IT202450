7448ba77f831268399f05d47612da31691a9e1a8
deziox <dezioxe@gmail.com>
2020-06-05 15:35:54 -0400

    Surveys (voting and results)

--- public_html/index.php
+++ public_html/index.php
@@ -21,17 +21,8 @@ if(isset($_SESSION['user'])){
     <meta charset="UTF-8">
     <title>Danzel Test Site</title>
     <script>
-        function getVote(top,bottom,id) {
+        function getVote() {
             alert("test");
-            /*
-            var xmlhttp=new XMLHttpRequest();
-            xmlhttp.onreadystatechange=function() {
-                if (this.readyState==4 && this.status==200) {
-                    document.getElementById("poll"+id).innerHTML=this.responseText;
-                }
-            }
-            xmlhttp.open("GET","vote.php?top="+top"&bottom="+bottom,true);
-            xmlhttp.send();*/
         }
     </script>
 </head>
