89d78fe3a4ae695753ecda7a250d44b46923b375
deziox <dezioxe@gmail.com>
2020-06-05 12:48:38 -0400

    Surveys

--- public_html/survey_list.php
+++ public_html/survey_list.php
@@ -1,30 +1,36 @@
-<?php
-    require('config.php');
-    session_start();
-
-    try{
-        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-        $db = new PDO($connection_string,$dbuser,$dbpass);
-
-
-        $stmt = $db->prepare("SELECT * FROM Surveys ORDER BY created_at");
-        $r = $stmt->execute();
-        $surveys = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        if(!$surveys){
-            echo "no surveys";
-        }else{
-            foreach($surveys as $s){
-                //echo var_export($s,true);
-                echo '<img class="clothes" src="data:image/png;base64,'.base64_encode($s['top_1_image']).'"/>';
-            }
-        }
-    }catch(Exception $e){
-        echo "Connection failed = ".$e->getMessage();
-    }
-?>
-
 <div class="survey">
+
+    <?php
+        require('config.php');
+        session_start();
+
+        try{
+            $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+            $db = new PDO($connection_string,$dbuser,$dbpass);
+
+
+            $stmt = $db->prepare("SELECT * FROM Surveys ORDER BY created_at");
+            $r = $stmt->execute();
+            $surveys = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if(!$surveys){
+                echo "no surveys";
+            }else{
+                echo '<table style="width:100%">';
+                foreach($surveys as $s){
+                    //echo var_export($s,true);
+                    echo '<tr>';
+                    echo '<th><img class="clothes" src="data:image/png;base64,'.base64_encode($s['top_1_image']).'"/></th>';
+                    echo '<th><img class="clothes" src="data:image/png;base64,'.base64_encode($s['top_2_image']).'"/></th>';
+                    echo '</tr>';
+                }
+                echo '</table>';
+            }
+        }catch(Exception $e){
+            echo "Connection failed = ".$e->getMessage();
+        }
+    ?>
+
     <div class="top">top</div>
     <div class="bottom">bottom</div>
 </div>
