07e1ff6c9818503e623385c73743ee086df83b39
deziox <dezioxe@gmail.com>
2020-06-09 13:13:27 -0400

    Login/Register with Session integration

--- public_html/register.php
+++ public_html/register.php
@@ -11,24 +11,25 @@
         <?php //include('header.php'); ?>
         <section>
             <h2>REGISTER AN ACCOUNT</h2>
-            <div>
+            <div class="register">
                 <h1>Already Have an account?</h1>
-                <input type="button" onclick="window.location.href='login.php'" value="Login"/>
+                <!--<input type="button" onclick="window.location.href='login.php'" value="Login"/>-->
+                <a class="login-button" href="#" data-target="survey_list">Login</a>
+                <form action="register.php" method="post">
+                    <label>Email:</label>
+                    <?php echo "<div class=\"error\">".$errors['email']."</div>";?>
+                    <input type="text" name="email"><br/>
+                    <label>Username:</label>
+                    <?php echo "<div class=\"error\">".$errors['username']."</div>";?>
+                    <input type="text" name="username"><br/>
+                    <label>Password:</label>
+                    <?php echo "<div class=\"error\">".$errors['password']."</div>";?>
+                    <input type="password" name="password"><br/>
+                    <div>
+                        <input type="submit" name="submit" value="Register">
+                    </div>
+                </form>
             </div>
-            <form action="register.php" method="post">
-                <label>Email:</label>
-                <?php echo "<div class=\"error\">".$errors['email']."</div>";?>
-                <input type="text" name="email"><br/>
-                <label>Username:</label>
-                <?php echo "<div class=\"error\">".$errors['username']."</div>";?>
-                <input type="text" name="username"><br/>
-                <label>Password:</label>
-                <?php echo "<div class=\"error\">".$errors['password']."</div>";?>
-                <input type="password" name="password"><br/>
-                <div>
-                    <input type="submit" name="submit" value="Register">
-                </div>
-            </form>
         </section>
     </body>
 </html>
