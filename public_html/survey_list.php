dca0942b23734fd2b116efebbe7a333cac529740
deziox <dezioxe@gmail.com>
2020-06-09 10:34:05 -0400

    Login/Register with Session integration

--- public_html/survey_list.php
+++ public_html/survey_list.php
@@ -16,7 +16,7 @@
             }else{
                 foreach($surveys as $s) {
                     //echo var_export($s,true);
-                    echo '<div class="survey'.$s['id'].'">';
+                    echo '<div class="survey" id="survey_'.$s['id'].'">';
                     echo '<form method="post" onsubmit="vote(top.value,bottom.value,'.$s['id'].')">';
                     echo '<h1 class="survey-title">' . $s['title'] . '</h1>';
                     echo '<table class="survey-table">';
