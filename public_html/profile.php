8c661481bf47b687b82a2c2e1e8b88bc0448c33e
deziox <dezioxe@gmail.com>
2020-06-09 15:52:27 -0400

    Login/Register with Session integration

--- public_html/profile.php
+++ public_html/profile.php
@@ -1,29 +1,5 @@
 <?php
 ?>
-
-
-<!DOCTYPE html>
-<html>
-<head></head>
-<body>
-<?php include('header.php'); ?>
-<section>
-    <h2>Login</h2>
-    <div>
-        <h4>Don't Have an account?</h4>
-        <input type="button" onclick="window.location.href='register.php'" value="Register"/>
-    </div>
-    <form action="login.php" method="post">
-        <label>Username:</label>
-        <?php echo "<div class=\"error\">".$errors['username']."</div>";?>
-        <input type="text" name="username"><br/>
-        <label>Password:</label>
-        <?php echo "<div class=\"error\">".$errors['password']."</div>";?>
-        <input type="password" name="password"><br/>
-        <div>
-            <input type="submit" name="submit" value="Login">
-        </div>
-    </form>
-</section>
-</body>
-</html>
+<div class="reglog-center">
+    <input class="login-button" type="button" onclick="window.location.href='logout.php'" value="logout"/>
+</div>
