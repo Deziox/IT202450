e6da473209998f5fedce7eef09edd80bf8a7ed62
deziox <dezioxe@gmail.com>
2020-06-09 15:57:13 -0400

    Login/Register with Session integration

--- public_html/logout.php
+++ public_html/logout.php
@@ -8,8 +8,6 @@
 
     if (ini_get("session.use_cookies")) {
         $params = session_get_cookie_params();
-        //clones then destroys since it makes it's lifetime
-        //negative (in the past)
         setcookie(session_name(), '', time() - 42000,
             $params["path"], $params["domain"],
             $params["secure"], $params["httponly"]
