9f4f94dcb0b60c24696c2a5e4a5583116a1ae705
deziox <dezioxe@gmail.com>
2020-06-16 11:30:51 -0400

    Creating a new survey

---
+++ public_html/Week5HW/create.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+require('config.php');
+
+$errors = array('title'=>'','top_1'=>'','top_1_image'=>'','top_2'=>'','top_2_image'=>'','bottom_1'=>'','bottom_1_image'=>'','bottom_2'=>'','bottom_2_image'=>'');
+
+if(isset($_POST['submit'])){
+    if(empty($_POST['title'])){
+        $errors['title'] = "A title is required for your aesthetic";
+    }else if(empty($_POST['top_1'])){
+        $errors['top_1'] = "A title is requried for first top";
+    }else if(empty($_POST['top_2'])){
+        $errors['top_2'] = "A title is requried for second top";
+    }else if(empty($_POST['top_1'])){
+        $errors['bottom_1'] = "A title is requried for first bottom";
+    }else if(empty($_POST['top_2'])){
+        $errors['bottom_2'] = "A title is requried for second bottom";
+    }else if(empty($_POST['top_1'])){
+        $errors['top_1_image'] = "An image is requried for first top";
+    }else if(empty($_POST['top_2'])){
+        $errors['top_2_image'] = "An image is requried for second top";
+    }else if(empty($_POST['top_1'])){
+        $errors['bottom_1_image'] = "An image is requried for first bottom";
+    }else if(empty($_POST['top_2'])){
+        $errors['bottom_2_image'] = "An image is requried for second bottom";
+    }else {
+        $title = $_POST['title'];
+        $top_1 = $_POST['top_1'];
+        $top_2 = $_POST['top_2'];
+        $bottom_1 = $_POST['bottom_1'];
+        $bottom_2 = $_POST['bottom_2'];
+        $top_1_image = $_POST['top_1_image'];
+        $top_2_image = $_POST['top_2_image'];
+        $bottom_1_image = $_POST['bottom_1_image'];
+        $bottom_2_image = $_POST['bottom_2_image'];
+
+        if($_POST['published'] == 'yes'){
+            $published = '1';
+        }else{
+            $published = '0';
+        }
+    }
+
+    //Database Managment
+    if(!array_filter($errors)){
+        //$result = $conn->query("SELECT id FROM Users WHERE email = $email");
+
+        try{
+            $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+            $db = new PDO($connection_string,$dbuser,$dbpass);
+
+            if(!array_filter($errors)){
+                $stmt = $db->prepare("INSERT INTO Surveys (user_id,title,top_1,top_1_image,top_2,top_2_image,bottom_1,bottom_1_image,bottom_2,bottom_2_image,published) VALUES 
+                                                                    (:user_id,:title,:top_1,:top_1_image,:top_2,:top_2_image,:bottom_1,:bottom_1_image,:bottom_2,:bottom_2_image,:published)");
+                $r = $stmt->execute(array(
+                    ":user_id"=>$_SESSION['user']['id'],
+                    ":title"=>$title,
+
+                    ":top_1"=>$top_1,
+                    ":top_1_image"=>$top_1_image,
+                    ":top_2"=>$top_2,
+                    ":top_2_image"=>$top_2_image,
+
+                    ":bottom_1"=>$bottom_1,
+                    ":bottom_1_image"=>$bottom_1_image,
+                    ":bottom_2"=>$bottom_2,
+                    ":bottom_2_image"=>$bottom_2_image,
+                    ":published"=>$published
+                ));
+                header("location:../index.php");
+            }
+
+        }catch(Exception $e){
+            echo "Connection failed = ".$e->getMessage();
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php include('header.php'); ?>
+<section class="reglog-center">
+    <h2>REGISTER AN ACCOUNT</h2>
+    <div class="register">
+        <div class="reglog-switch">
+            <h3>Create Survey Week 5 HW</h3>
+        </div>
+        <form action="create.php" method="post">
+            <label>aesthetic title:</label>
+            <?php echo "<div class=\"error\">".$errors['title']."</div>";?>
+            <input type="text" name="title"><br/>
+
+            <label>First Top</label>
+            <?php echo "<div class=\"error\">".$errors['top_1']."</div>";?>
+            <input type="text" name="top_1"><br/>
+            <input type="file" name="top_1_image"> <?php echo "<div class=\"error\">".$errors['top_1_image']."</div>";?> <br/>
+
+            <label>Second Top</label>
+            <?php echo "<div class=\"error\">".$errors['top_2']."</div>";?>
+            <input type="text" name="top_2"><br/>
+            <input type="file" name="top_2_image"> <?php echo "<div class=\"error\">".$errors['top_2_image']."</div>";?> <br/>
+
+            <label>First Bottom</label>
+            <?php echo "<div class=\"error\">".$errors['bottom_1']."</div>";?>
+            <input type="text" name="bottom_1"><br/>
+            <input type="file" name="bottom_1_image"> <?php echo "<div class=\"error\">".$errors['bottom_1_image']."</div>";?> <br/>
+
+            <label>Second Bottom</label>
+            <?php echo "<div class=\"error\">".$errors['bottom_2']."</div>";?>
+            <input type="text" name="bottom_2"><br/>
+            <input type="file" name="bottom_2_image"> <?php echo "<div class=\"error\">".$errors['bottom_2_image']."</div>";?> <br/>
+
+            <label>Published</label>
+            <input type="checkbox" name="published" value="yes"><br/>
+
+            <div style="margin: 20px;">
+                <input class="register-button" type="submit" name="submit" value="find aesthetic">
+            </div>
+        </form>
+    </div>
+</section>
+</body>
+</html>
---
+++ public_html/Week5HW/header.php
@@ -0,0 +1,49 @@
+<?php
+    if(isset($_SESSION['user'])){
+        $logreg = $_SESSION['user']['username'];
+        $logregdatatarget = "profile";
+    }else{
+        $logreg = "login/register";
+        $logregdatatarget = "register";
+    }
+?>
+
+<head>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Space+Mono">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/content.css">
+    <link rel="stylesheet" href="css/surveys.css">
+    <link rel="stylesheet" href="css/login.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+           //trigger/container variables
+           var trigger = $('.header-right'), container = $('.content');
+
+           //fire on click
+           trigger.on('click','#nav',function(){
+               var $this = $(this), target = $this.data('target');
+               container.load(target + '.php');
+               document.getElementsByClassName("active").item(0).removeAttribute("class");
+               document.getElementsByName(target).item(0).className = "active";
+               document.getElementsByTagName("title").item(0).innerHTML = "aestheticus|" + target;
+               return false;
+           });
+        });
+    </script>
+</head>
+<div class="header">
+    <a id="nav" href="index.php" data-target="survey_list" class="logo">a e s t h e t i c u s</a>
+    <div class="header-right">
+        <a id="nav" name="survey_list" href="index.php" data-target="survey_list" class="active">home</a>
+        <a id="nav" name="outfits" href="#" data-target="outfits">outfits</a>
+        <?php
+            if(isset($_SESSION['user'])) {
+                echo '<a id="nav" name="profile" href="#" data-target="profile">'.$_SESSION["user"]["username"].'</a>';
+            }else{
+                echo '<a id="nav" name="register" href="register.php">login/register</a>';
+            }
+        ?>
+    </div>
+</div>
