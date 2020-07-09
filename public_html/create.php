da8b16bec3c3ebbdec13d74891994d29a1a72b4e
deziox <danzel.serrano@gmail.com>
2020-07-09 16:49:12 -0400

    Milestone 3

--- public_html/create.php
+++ public_html/create.php
@@ -1,18 +1,5 @@
 <?php
-require('../vendor/autoload.php');
-// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = new Aws\S3\S3Client([
-    'version'  => 'latest',
-    'region'   => 'us-east-1',
-    'credentials' => [
-        'key'    => getenv('AWS_ACCESS_KEY_ID'),
-        'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
-    ]
-]);
-$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
-?>
-
-<?php
+include("aws_config.php");
 session_start();
 require('config.php');
 //print_r($_FILES);
--- public_html/profile.php
+++ public_html/profile.php
@@ -1,9 +1,19 @@
 <?php
-    include("header.php");
+    if(!isset($_GET['profile_id'])){
+        if(isset($_SESSION['user'])) {
+            header("location: profile.php?profile_id=".$_SESSION['user']['id']);
+        }else{
+            header("location: index.php");
+        }
+    }
+    include("aws_config.php");
+    $profile_id = $_GET['profile_id'];
 ?>
 <html>
 <head></head>
 <body>
+    <?php include("header.php");?>
+
     <div class="reglog-center">
         <input class="login-button redtext" type="button" onclick="window.location.href='logout.php'" value="logout"/>
     </div>
