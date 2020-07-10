e8d53a70c49f6cc5a1ffd9d9a2e80a67aca0b177
deziox <danzel.serrano@gmail.com>
2020-07-09 22:59:10 -0400

    Milestone 3

--- public_html/profile.php
+++ public_html/profile.php
@@ -9,11 +9,27 @@
     }
     include("aws_config.php");
     $profile_id = $_GET['profile_id'];
+
+    $result = $s3->listObjects(array('Bucket'=>'aestheticus'));
+
+    $profile_img = 'https://aestheticus.s3.amazonaws.com/defaultprofile.jpg';
+    foreach($result['Contents'] as $object){
+        //echo var_export($object).'\n';
+        if (strpos($object['Key'],$profile_id.'_profile') !== false) {
+            $profile_img = 'https://aestheticus.s3.amazonaws.com/' . $object['Key'];
+            break;
+        }
+    }
 ?>
 <html>
 <head></head>
 <body>
     <?php include("header.php");?>
+
+    <div class="profile-container">
+        <div class="profile-img"><?php echo '<img src="'.$profile_img.'"';?></div>
+        <div class="profile-bio"></div>
+    </div>
 
     <div class="reglog-center">
         <input class="login-button redtext" type="button" onclick="window.location.href='logout.php'" value="logout"/>
