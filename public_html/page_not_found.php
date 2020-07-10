0a71678995e7d00068ad33fd065bf95b6c83a59d
deziox <danzel.serrano@gmail.com>
2020-07-09 23:21:44 -0400

    Milestone 3

---
+++ public_html/page_not_found.php
@@ -0,0 +1,4 @@
+<?php
+    include("header.php");
+    echo "<h1 style='text-align:center;text-effect: emboss;'>404 PAGE NOT FOUND</h1>"
+?>
--- public_html/profile.php
+++ public_html/profile.php
@@ -10,6 +10,22 @@
     include("aws_config.php");
     $profile_id = $_GET['profile_id'];
 
+    try{
+        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+        $db = new PDO($connection_string,$dbuser,$dbpass);
+
+
+        $stmt = $db->prepare("SELECT * FROM Users WHERE id = :id");
+        $r = $stmt->execute(array(":id"=>$profile_id));
+        $userresult = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if(!$userresult) {header("location: page_not_found.php");}
+        $uname = $userresult['username'];
+
+    }catch(Exception $e){
+        echo "Connection failed = ".$e->getMessage();
+    }
+
     $result = $s3->listObjects(array('Bucket'=>'aestheticus'));
 
     $profile_img = 'https://aestheticus.s3.amazonaws.com/defaultprofile.jpg';
@@ -27,6 +43,7 @@
     <?php include("header.php");?>
 
     <div class="profile-container">
+        <?php echo '<h3 class="profile-name">'.$uname.'</h3>'?>
         <div class="profile-img"><?php echo '<img class="profile-img" src="'.$profile_img.'"';?></div>
         <div class="profile-bio"></div>
     </div>
