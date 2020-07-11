47b8699b29715f1aa082c03f280c2178ce5116c9
deziox <danzel.serrano@gmail.com>
2020-07-10 21:09:30 -0400

    Milestone 3 (password reset) (Added PHPMailer)

---
+++ public_html/forgot_password.php
@@ -0,0 +1,52 @@
+<?php
+    if(isset($_POST['submit'])){
+        if(empty($_POST['email'])){
+            $errors['email'] = "Email cannot be empty";
+        }
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = "Not a Valid Email";
+        }
+
+        if(!array_filter($errors)){
+            include('mailer-config.php');
+            $alphnum = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $rcode = '';
+            for($i = 0; $i < 6; $i++){
+                $random_char = $alphnum[mt_rand(0,strlen($alphnum) - 1)];
+                $rcode .= $random_char;
+            }
+
+            $mail->Subject = 'a e s t h e t i c u s password reset code';
+            $mail->Body = '<html>
+                                <h1>your password reset code: </h1>
+                                '.$rcode.'
+                           </html>';
+            $mail->AddAddress('dezioxe@gmail.com');
+
+            $mail->send();
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php include('header.php'); ?>
+<section class="reglog-center">
+    <h2>Login</h2>
+    <div class="login">
+
+        <form action="login.php" method="post">
+            <label>Forgot your password? Enter your email: </label>
+            <?php echo "<div class=\"error\">".$errors['email']."</div>";?>
+            <input type="text" name="email"><br/>
+            <div style="margin: 20px;">
+                <input class="login-button" type="submit" name="submit" value="send code">
+            </div>
+        </form>
+    </div>
+</section>
+</body>
+</html>
--- public_html/mailer-config.php
+++ public_html/mailer-config.php
@@ -11,9 +11,10 @@
     $mail->Username = 'aestheticushelp@gmail.com';
     $mail->Password = '@$sa4G<KU_aLN&\'97';
     $mail->SetFrom('no-reply@aestheticus.com');
-    $mail->Subject = 'Test email from aestheticus';
-    $mail->Body = '<html><h1>THIS IS A TEST EMAIL, no memes</h1></html>';
-    $mail->AddAddress('dezioxe@gmail.com');
 
-    $mail->send();
+    //$mail->Subject = 'Test email from aestheticus';
+    //$mail->Body = '<html><h1>THIS IS A TEST EMAIL, no memes</h1></html>';
+    //$mail->AddAddress('dezioxe@gmail.com');
+    //
+    //$mail->send();
 ?>
