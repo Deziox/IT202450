151a12f122017221941d98fd085af6fd766539d1
deziox <danzel.serrano@gmail.com>
2020-07-13 15:21:03 -0400

    Milestone 3 (profile and profile edit)

--- public_html/delete_survey.php
+++ public_html/delete_survey.php
@@ -4,36 +4,37 @@ require("config.php");
 
 if(!isset($_SESSION['user'])){
     header('location: index.php');
-}
-session_start();
+}else {
+    session_start();
 
-if(!isset($_GET['id'])){
-    header('location: index.php');
-}
+    if (!isset($_GET['id'])) {
+        header('location: index.php');
+    }else {
+        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 
-$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+        try {
+            $db = new PDO($connection_string, $dbuser, $dbpass);
 
-try {
-    $db = new PDO($connection_string, $dbuser, $dbpass);
+            $surveys = explode(',', $_SESSION['user']['surveys']);
+            echo var_export($surveys);
+            if (!in_array($_GET['id'], $surveys)) {
+                //header('location: index.php');
+                echo 'test 1';
+            }
 
-    $surveys = explode(',',$_SESSION['user']['surveys']);
-    echo var_export($surveys);
-    if(!in_array($_GET['id'],$surveys)){
-        //header('location: index.php');
-        echo 'test 1';
+            $stmt = $db->prepare("DELETE FROM Surveys WHERE id = :id");
+            $r = $stmt->execute(array(":id" => $_GET['id']));
+
+            unset($surveys[array_search($_GET['id'], $surveys)]);
+            $_SESSION['user']['surveys'] = $surveys;
+
+            $stmt = $db->prepare("UPDATE Users SET surveys = :surveys WHERE id = :id");
+            $r = $stmt->execute(array(":surveys" => join(',', $surveys), ":id" => $_SESSION['user']['id']));
+
+            //header('location: index.php');
+            echo 'test 2';
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
-
-    $stmt = $db->prepare("DELETE FROM Surveys WHERE id = :id");
-    $r = $stmt->execute(array(":id" => $_GET['id']));
-
-    unset($surveys[array_search($_GET['id'],$surveys)]);
-    $_SESSION['user']['surveys'] = $surveys;
-
-    $stmt = $db->prepare("UPDATE Users SET surveys = :surveys WHERE id = :id");
-    $r = $stmt->execute(array(":surveys"=>join(',',$surveys),":id" => $_SESSION['user']['id']));
-
-    //header('location: index.php');
-    echo 'test 2';
-} catch (Exception $e) {
-    echo $e->getMessage();
 }
