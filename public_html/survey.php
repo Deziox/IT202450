a602e50faf8055084dd3d9127a7589521248bd0e
deziox <danzel.serrano@gmail.com>
2020-07-14 14:57:10 -0400

    Milestone 3 (Core Features)

--- public_html/survey.php
+++ public_html/survey.php
@@ -42,7 +42,12 @@ include("aws_config.php");
                 $s = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
 
                 if($s['published'] !== 2 || $s['approved'] !== 1){
-                    header('location: index.php');
+                    //header('location: index.php');
+                    echo var_export($s);
+                    echo "\n".($s['published'] !== 2);
+                    echo "\n".($s['approved'] !== 1);
+                    echo "\n".($s['published'] != 2);
+                    echo "\n".($s['approved'] != 1);
                 }else {
 
                     $top1_bottom1 = $s['top1_bottom1'];
