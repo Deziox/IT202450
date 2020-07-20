dc3c56fe8869a467b7bba7dda995f1b4b4349c10
deziox <danzel.serrano@gmail.com>
2020-07-20 06:02:01 -0400

    Final Project Proposal (profile editing)

--- public_html/edit_profile.php
+++ public_html/edit_profile.php
@@ -1,15 +1,13 @@
 <?php
 require('config.php');
 session_start();
-if(!isset($_GET['profile_id'])){
-    if(isset($_SESSION['user'])) {
-        header("location: profile.php?profile_id=".$_SESSION['user']['id']);
-    }else{
-        header("location: index.php");
-    }
+if(!isset($_SESSION['user'])) {
+    header("location: index.php");
+    session_abort();
+    die();
 }
 include("aws_config.php");
-$profile_id = $_GET['profile_id'];
+$profile_id = $_SESSION['user']['id'];
 
 try{
     $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
@@ -19,9 +17,6 @@ try{
     $stmt = $db->prepare("SELECT * FROM Users WHERE id = :id");
     $r = $stmt->execute(array(":id"=>$profile_id));
     $userresult = $stmt->fetch(PDO::FETCH_ASSOC);
-    if($userresult['id'] !== $_GET['profile_id']){
-        header("location: page_not_found.php");
-    }
 
     $uname = $userresult['username'];
     $bio = $userresult['bio'];
@@ -46,41 +41,24 @@ foreach($result['Contents'] as $object){
 <head>
     <link rel="stylesheet" href="css/profile.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
 </head>
 <body>
 <?php include("header.php");?>
 
+<form action="edit_profile.php" method="post" enctype="multipart/form-data">
+
 <div class="profile-container">
-    <?php echo '<h1 class="profile-name">'.$uname.'</h1>'?>
+    <h3 style="text-align: center;text-decoration: underline;">username:</h3>
+    <?php echo '<h1 class="profile-name"><input type="text" name="username" value="'.$uname.'" style="text-align: center;font-size: 30px;"></h1>'?>
     <div class="profile-img"><?php echo '<img class="profile-img" src="'.$profile_img.'"';?></div>
-    <div class="profile-bio"><?php echo '<h3 class="profile-bio">'.$bio.'</h3>'?></div>
-    <?php
-    if(isset($_SESSION['user'])) {
-        if($_SESSION['user']['id'] === $_GET['profile_id']) {
-            echo '
-                <div class="reglog-center" >
-                    <input class="login-button redtext" type = "button" onclick = "window.location.href=\'edit_profile.php?profile_id='.$profile_id.'\'" value = "edit profile" />
-                </div >
-                <div class="reglog-center" >
-                    <input class="login-button redtext" type = "button" onclick = "window.location.href=\'logout.php\'" value = "logout" />
-                </div >';
-        }
-    }
-    ?>
+    <h3 style="text-align: center;text-decoration: underline;">bio:</h3>
+    <div class="profile-bio"><?php echo '<h3 class="profile-bio"><textarea name="bio" rows="8" cols="80" style="text-align: center;font-size: 20px;">'.$bio.'</textarea></h3>'?></div>
 </div>
 
 <?php
-if(isset($_GET['profile_id']))
+$query = "SELECT * FROM Surveys WHERE user_id = :user_id ORDER BY created_at DESC";
 
-    $query = "SELECT * FROM Surveys WHERE user_id = :user_id AND (published = 2 OR published = 1) AND approved = 1 ORDER BY created_at DESC";
-if(isset($_SESSION['user'])) {
-    if($_SESSION['user']['id'] === $_GET['profile_id']) {
-        $query = "SELECT * FROM Surveys WHERE user_id = :user_id ORDER BY created_at DESC";
-    }
-}
-$h = $uname."'s outfits";
-
+$h = "edit your outfits:";
 try{
 //        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 //        $db = new PDO($connection_string,$dbuser,$dbpass);
@@ -90,7 +68,7 @@ try{
     $surveys = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     if(!$surveys){
-        $h = $uname." has no public outfits";
+        $h = "you have no created outfits";
         echo '<h1 class="content-header">'.$h.'</h1><hr>';
     }else {
         echo '<h1 class="content-header">' . $h . '</h1><hr>';
@@ -111,10 +89,10 @@ try{
                 }
             }
 
-            echo '<div class="survey" id="survey_' . $s['id'] . '">';
+            echo '<a href="edit_survey.php?id=' . $s['id'] . '"><div class="survey" id="survey_' . $s['id'] . '">';
             echo '<table class="survey-table">';
             echo '<div style="margin-left: 94%; margin-top: 1.25%;">
-                    <a href="#"><img src="images/1214428.svg" style="height: 5%;"></a>
+                    <a href="#" id="delete"><img src="images/1214428.svg" style="height: 5%;"></a>
                     </div>';
             echo '<tr> <h1 class="profile-survey-title">' . $s['title'] . '</h1>
                         <th>
@@ -135,7 +113,7 @@ try{
 
             echo '</table>';
             echo '<div id="poll' . $s['id'] . '"></div>';
-            echo '</div>';
+            echo '</div></a>';
         }
     }
 
@@ -143,7 +121,7 @@ try{
         echo "Connection failed = ".$e->getMessage();
     }
     ?>
-
+</form>
     <?php include('footer.php'); ?>
 </body>
 </html>
